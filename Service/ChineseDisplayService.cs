b20fe20812292aebaa8740abf66c65373914289b
Echo Lee <bdottn.lee+git@gmail.com>
2017-07-02 14:20:23 +0800

    建立 ChineseDisplayService
    1.　繼承 IDisplayService
    　1.1　實作方法：GetFullName
    　1.2　實作方法：GetShortName

---
+++ Service/ChineseDisplayService.cs
@@ -0,0 +1,68 @@
+﻿using Service.Protocol;
+using System;
+
+namespace Service
+{
+    public sealed class ChineseDisplayService : IDisplayService
+    {
+        public string GetFullName(DayOfWeek dayOfWeek)
+        {
+            switch (dayOfWeek)
+            {
+                case DayOfWeek.Sunday:
+                    return "星期日";
+
+                case DayOfWeek.Monday:
+                    return "星期一";
+
+                case DayOfWeek.Tuesday:
+                    return "星期二";
+
+                case DayOfWeek.Wednesday:
+                    return "星期三";
+
+                case DayOfWeek.Thursday:
+                    return "星期四";
+
+                case DayOfWeek.Friday:
+                    return "星期五";
+
+                case DayOfWeek.Saturday:
+                    return "星期六";
+
+                default:
+                    return string.Empty;
+            }
+        }
+
+        public string GetShortName(DayOfWeek dayOfWeek)
+        {
+            switch (dayOfWeek)
+            {
+                case DayOfWeek.Sunday:
+                    return "日";
+
+                case DayOfWeek.Monday:
+                    return "一";
+
+                case DayOfWeek.Tuesday:
+                    return "二";
+
+                case DayOfWeek.Wednesday:
+                    return "三";
+
+                case DayOfWeek.Thursday:
+                    return "四";
+
+                case DayOfWeek.Friday:
+                    return "五";
+
+                case DayOfWeek.Saturday:
+                    return "六";
+
+                default:
+                    return string.Empty;
+            }
+        }
+    }
+}
