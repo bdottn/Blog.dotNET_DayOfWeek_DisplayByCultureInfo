901fb03ed01315fa5c7363fe04dd857f29b92453
Echo Lee <bdottn.lee+git@gmail.com>
2017-07-02 14:17:56 +0800

    建立 IDisplayService
    1.　加入方法：GetFullName
    2.　加入方法：GetShortName

---
+++ Service/Protocol/IDisplayService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Service.Protocol
+{
+    public interface IDisplayService
+    {
+        string GetFullName(DayOfWeek dayOfWeek);
+
+        string GetShortName(DayOfWeek dayOfWeek);
+    }
+}
