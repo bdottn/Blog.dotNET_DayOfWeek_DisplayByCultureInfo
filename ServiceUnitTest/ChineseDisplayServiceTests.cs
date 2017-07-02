324d03b0b866e499beb223bb304a31d87abf9cf7
Echo Lee <bdottn.lee+git@gmail.com>
2017-07-02 14:21:53 +0800

    建立 ChineseDisplayServiceTests 單元測試
    1.　加入測試案例：GetFullNameTest_傳入Tuesday_預期回傳星期二
    2.　加入測試案例：GetFullNameTest_傳入Thursday_預期回傳星期四
    3.　加入測試案例：GetShortNameTest_傳入Monday_預期回傳一
    4.　加入測試案例：GetShortNameTest_傳入Saturday_預期回傳六

---
+++ ServiceUnitTest/ChineseDisplayServiceTests.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Service.Protocol;
+using System;
+
+namespace Service.UnitTest
+{
+    [TestClass]
+    public sealed class ChineseDisplayServiceTests
+    {
+        private IDisplayService service;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            this.service = new ChineseDisplayService();
+        }
+
+        [TestMethod]
+        public void GetFullNameTest_傳入Tuesday_預期回傳星期二()
+        {
+            var dayOfWeek = DayOfWeek.Tuesday;
+
+            var expected = "星期二";
+
+            var actual = this.service.GetFullName(dayOfWeek);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetFullNameTest_傳入Thursday_預期回傳星期四()
+        {
+            var dayOfWeek = DayOfWeek.Thursday;
+
+            var expected = "星期四";
+
+            var actual = this.service.GetFullName(dayOfWeek);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetShortNameTest_傳入Monday_預期回傳一()
+        {
+            var dayOfWeek = DayOfWeek.Monday;
+
+            var expected = "一";
+
+            var actual = this.service.GetShortName(dayOfWeek);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetShortNameTest_傳入Saturday_預期回傳六()
+        {
+            var dayOfWeek = DayOfWeek.Saturday;
+
+            var expected = "六";
+
+            var actual = this.service.GetShortName(dayOfWeek);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
