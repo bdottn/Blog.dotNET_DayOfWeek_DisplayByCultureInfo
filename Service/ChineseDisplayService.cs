3831ef729d16bb0b44d01340934b2906bfa63ae0
Echo Lee <bdottn.lee+git@gmail.com>
2017-07-02 14:37:04 +0800

    調整 ChineseDisplayService、JapaneseDisplayService
    1.　實作方法：GetFullName，使用 CultureInfo
    2.　實作方法：GetShortName，使用 CultureInfo

--- Service/ChineseDisplayService.cs
+++ Service/ChineseDisplayService.cs
@@ -1,5 +1,6 @@
 ﻿using Service.Protocol;
 using System;
+using System.Globalization;
 
 namespace Service
 {
@@ -7,62 +8,12 @@ namespace Service
     {
         public string GetFullName(DayOfWeek dayOfWeek)
         {
-            switch (dayOfWeek)
-            {
-                case DayOfWeek.Sunday:
-                    return "星期日";
-
-                case DayOfWeek.Monday:
-                    return "星期一";
-
-                case DayOfWeek.Tuesday:
-                    return "星期二";
-
-                case DayOfWeek.Wednesday:
-                    return "星期三";
-
-                case DayOfWeek.Thursday:
-                    return "星期四";
-
-                case DayOfWeek.Friday:
-                    return "星期五";
-
-                case DayOfWeek.Saturday:
-                    return "星期六";
-
-                default:
-                    return string.Empty;
-            }
+            return new CultureInfo("zh-TW").DateTimeFormat.GetDayName(dayOfWeek);
         }
 
         public string GetShortName(DayOfWeek dayOfWeek)
         {
-            switch (dayOfWeek)
-            {
-                case DayOfWeek.Sunday:
-                    return "日";
-
-                case DayOfWeek.Monday:
-                    return "一";
-
-                case DayOfWeek.Tuesday:
-                    return "二";
-
-                case DayOfWeek.Wednesday:
-                    return "三";
-
-                case DayOfWeek.Thursday:
-                    return "四";
-
-                case DayOfWeek.Friday:
-                    return "五";
-
-                case DayOfWeek.Saturday:
-                    return "六";
-
-                default:
-                    return string.Empty;
-            }
+            return new CultureInfo("zh-TW").DateTimeFormat.GetShortestDayName(dayOfWeek);
         }
     }
 }
--- Service/JapaneseDisplayService.cs
+++ Service/JapaneseDisplayService.cs
@@ -1,5 +1,6 @@
 ﻿using Service.Protocol;
 using System;
+using System.Globalization;
 
 namespace Service
 {
@@ -7,62 +8,12 @@ namespace Service
     {
         public string GetFullName(DayOfWeek dayOfWeek)
         {
-            switch (dayOfWeek)
-            {
-                case DayOfWeek.Sunday:
-                    return "日曜日";
-
-                case DayOfWeek.Monday:
-                    return "月曜日";
-
-                case DayOfWeek.Tuesday:
-                    return "火曜日";
-
-                case DayOfWeek.Wednesday:
-                    return "水曜日";
-
-                case DayOfWeek.Thursday:
-                    return "木曜日";
-
-                case DayOfWeek.Friday:
-                    return "金曜日";
-
-                case DayOfWeek.Saturday:
-                    return "土曜日";
-
-                default:
-                    return string.Empty;
-            }
+            return new CultureInfo("ja-JP").DateTimeFormat.GetDayName(dayOfWeek);
         }
 
         public string GetShortName(DayOfWeek dayOfWeek)
         {
-            switch (dayOfWeek)
-            {
-                case DayOfWeek.Sunday:
-                    return "日";
-
-                case DayOfWeek.Monday:
-                    return "月";
-
-                case DayOfWeek.Tuesday:
-                    return "火";
-
-                case DayOfWeek.Wednesday:
-                    return "水";
-
-                case DayOfWeek.Thursday:
-                    return "木";
-
-                case DayOfWeek.Friday:
-                    return "金";
-
-                case DayOfWeek.Saturday:
-                    return "土";
-
-                default:
-                    return string.Empty;
-            }
+            return new CultureInfo("ja-JP").DateTimeFormat.GetShortestDayName(dayOfWeek);
         }
     }
 }
