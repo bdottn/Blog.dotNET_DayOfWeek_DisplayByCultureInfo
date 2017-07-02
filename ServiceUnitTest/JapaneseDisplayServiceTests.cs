22b008be0bf5cb98c6da0f7c42a4901d1746a36f
Echo Lee <bdottn.lee+git@gmail.com>
2017-07-02 14:26:06 +0800

    建立 JapaneseDisplayServiceTests 單元測試
    1.　加入測試案例：GetFullNameTest_傳入Monday_預期回傳月曜日
    2.　加入測試案例：GetFullNameTest_傳入Friday_預期回傳金曜日
    3.　加入測試案例：GetShortNameTest_傳入Wednesday_預期回傳水
    4.　加入測試案例：GetShortNameTest_傳入Sunday_預期回傳日

---
+++ ServiceUnitTest/JapaneseDisplayServiceTests.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Service.Protocol;
+using System;
+
+namespace Service.UnitTest
+{
+    [TestClass]
+    public sealed class JapaneseDisplayServiceTests
+    {
+        private IDisplayService service;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            this.service = new JapaneseDisplayService();
+        }
+
+        [TestMethod]
+        public void GetFullNameTest_傳入Monday_預期回傳月曜日()
+        {
+            var dayOfWeek = DayOfWeek.Monday;
+
+            var expected = "月曜日";
+
+            var actual = this.service.GetFullName(dayOfWeek);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetFullNameTest_傳入Friday_預期回傳金曜日()
+        {
+            var dayOfWeek = DayOfWeek.Friday;
+
+            var expected = "金曜日";
+
+            var actual = this.service.GetFullName(dayOfWeek);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetShortNameTest_傳入Wednesday_預期回傳水()
+        {
+            var dayOfWeek = DayOfWeek.Wednesday;
+
+            var expected = "水";
+
+            var actual = this.service.GetShortName(dayOfWeek);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetShortNameTest_傳入Sunday_預期回傳日()
+        {
+            var dayOfWeek = DayOfWeek.Sunday;
+
+            var expected = "日";
+
+            var actual = this.service.GetShortName(dayOfWeek);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
