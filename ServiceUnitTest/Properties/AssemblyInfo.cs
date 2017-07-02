bc57a3d3379347c2719021961ca9e1ccd8b12931
Echo Lee <bdottn.lee+git@gmail.com>
2017-07-02 13:57:45 +0800

    Solution initial commit
    1.　建立 【.NET】DayOfWeek DisplayByCultureInfo 方案
    
    2.　新增 Service 類別庫專案
    
    3.　新增 ServiceUnitTest 單元測試專案
    　3.1　加入專案參考：Service

---
+++ ServiceUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ServiceUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("ServiceUnitTest")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("de74729a-81c1-4640-b20f-fc407702aea0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
