2efb0d64fe07ae16dd505cd4c066908be3e3416d
Echo Lee <bdottn.lee+git@gmail.com>
2017-07-02 14:03:29 +0800

    建立 DisplayService
    1.　加入方法：GetFullName

---
+++ Service/DisplayService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Service
+{
+    public sealed class DisplayService
+    {
+        public string GetFullName(DayOfWeek dayOfWeek)
+        {
+            switch (dayOfWeek)
+            {
+                case DayOfWeek.Sunday:
+                    return "星期日";
+
+                case DayOfWeek.Monday:
+                    return "星期一";
+
+                case DayOfWeek.Tuesday:
+                    return "星期二";
+
+                case DayOfWeek.Wednesday:
+                    return "星期三";
+
+                case DayOfWeek.Thursday:
+                    return "星期四";
+
+                case DayOfWeek.Friday:
+                    return "星期五";
+
+                case DayOfWeek.Saturday:
+                    return "星期六";
+
+                default:
+                    return string.Empty;
+            }
+        }
+    }
+}
