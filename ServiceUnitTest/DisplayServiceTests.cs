938fc54a086748047c7b22bf84deecc7397ce7fd
Echo Lee <bdottn.lee+git@gmail.com>
2017-07-02 14:15:54 +0800

    建立 DisplayServiceTests 單元測試
    1.　加入測試案例：GetFullNameTest_傳入Sunday_預期回傳星期日
    2.　加入測試案例：GetFullNameTest_傳入Friday_預期回傳星期五

---
+++ ServiceUnitTest/DisplayServiceTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Service.UnitTest
+{
+    [TestClass]
+    public sealed class DisplayServiceTests
+    {
+        private DisplayService service;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            this.service = new DisplayService();
+        }
+
+        [TestMethod]
+        public void GetFullNameTest_傳入Sunday_預期回傳星期日()
+        {
+            var dayOfWeek = DayOfWeek.Sunday;
+
+            var expected = "星期日";
+
+            var actual = this.service.GetFullName(dayOfWeek);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetFullNameTest_傳入Friday_預期回傳星期五()
+        {
+            var dayOfWeek = DayOfWeek.Friday;
+
+            var expected = "星期五";
+
+            var actual = this.service.GetFullName(dayOfWeek);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
