d6c0fdcd3faf8ab046a4309b36d0a170b7dc7e7c
Echo Lee <bdottn.lee+git@gmail.com>
2017-07-02 14:33:21 +0800

    調整 DisplayServiceTests 單元測試
    1.　加入測試案例：GetShortNameTest_傳入Wednesday_預期回傳三
    2.　加入測試案例：GetShortNameTest_傳入Thursday_預期回傳四

--- ServiceUnitTest/DisplayServiceTests.cs
+++ ServiceUnitTest/DisplayServiceTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Service.Protocol;
 using System;
 
 namespace Service.UnitTest
@@ -6,7 +7,7 @@ namespace Service.UnitTest
     [TestClass]
     public sealed class DisplayServiceTests
     {
-        private DisplayService service;
+        private IDisplayService service;
 
         [TestInitialize]
         public void TestInitialize()
@@ -34,6 +35,30 @@ namespace Service.UnitTest
             var expected = "星期五";
 
             var actual = this.service.GetFullName(dayOfWeek);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetShortNameTest_傳入Wednesday_預期回傳三()
+        {
+            var dayOfWeek = DayOfWeek.Wednesday;
+
+            var expected = "三";
+
+            var actual = this.service.GetShortName(dayOfWeek);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetShortNameTest_傳入Thursday_預期回傳四()
+        {
+            var dayOfWeek = DayOfWeek.Thursday;
+
+            var expected = "四";
+
+            var actual = this.service.GetShortName(dayOfWeek);
 
             Assert.AreEqual(expected, actual);
         }
